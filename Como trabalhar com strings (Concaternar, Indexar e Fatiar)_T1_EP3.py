226d3e1ebacaea7b370f33f39b194d76c4acc74e
BrunoOJ <92693586+BrunoOJ@users.noreply.github.com>
2023-10-26 21:14:49 -0300

    Add files via upload

---
+++ Como trabalhar com strings (Concaternar, Indexar e Fatiar)_T1_EP3.py
@@ -0,0 +1,79 @@
+'''
+PYTHON_STRING_T1_EP3
+
+'''
+
+var1 = 'como utilizar'
+var2 = "string no python"
+
+print(var1)
+print(var2)
+
+
+#-------------------------------------------
+
+##'''1.1. Concatenação de strings: Você pode juntar duas ou mais
+##strings usando o operador de adição (+).'''
+##
+##frase_sem_espaco = var1+var2
+##print(frase_sem_espaco)
+##nome_completo = var1 + " " + var2
+##print(nome_completo)
+##
+
+#-------------------------------------------
+
+##'''2.2. Indexação de caracteres: Você pode acessar caracteres
+##individuais em uma string usando índices. Lembre-se de que os
+##índices em Python começam em (0) se considerar a ordem da esquerda
+##para a direita (-1) da direita para a esquerda.'''
+##
+##frase = "Python"
+##primeira_letra = frase[0]  
+##segunda_letra = frase[1]   
+##ultima_letra = frase[-1]
+##ultima_letra1 = frase[5]
+##var3 = frase[-3]
+##print(primeira_letra)
+##print(segunda_letra)
+##print(ultima_letra)
+##print(ultima_letra1)
+##print(var3)
+
+#-------------------------------------------
+
+##'''3.3. Fatias de strings (slicing): Você pode obter partes de
+##uma string usando fatias (slices). O formato é string[inicio:fim],
+##onde inicio é o índice do primeiro caractere incluído na fatia, e
+##fim é o índice do primeiro caractere excluído da fatia.'''
+##
+##frase = "Python é uma linguagem de programação."
+##parte_frase = frase[0:6]
+##var5 = frase[26:-1]
+##print(var5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
