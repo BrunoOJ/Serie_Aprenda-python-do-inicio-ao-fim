76232cda9b024205be0c97de69e5d807b998dce2
BrunoOJ <92693586+BrunoOJ@users.noreply.github.com>
2023-11-02 21:54:36 -0300

    Add files via upload

---
+++ Como manipular String no Python (MAIÚSCULAS E minúsculas)_T1_EP4.py
@@ -0,0 +1,53 @@
+'''
+PYTHON_STRING_T1_EP4 - Maiúsculas e Minúsculas
+
+'''
+
+## lower(): Converte todos os caracteres da string para minúsculas.
+texto = "OLÁ, MUNDO!"
+print(texto.lower())
+
+#### upper(): Converte todos os caracteres da string para maiúsculas.
+##texto = "Olá, Mundo!"
+##print(texto.upper())
+
+####
+#### capitalize(): Converte o primeiro caractere da string para
+#### maiúscula e o restante para minúscula.
+##texto = "olá, mundo!"
+##print(texto.capitalize())
+
+#### casefold(): Retorna uma versão em minúsculas da string.
+##texto = "Olá, Mundo!"
+##print(texto.casefold())  
+
+
+#### islower(): Verifica se todos os caracteres da string estão em minúsculas.
+##texto = "pythoN"
+##print(texto.islower()) 
+##
+#### isupper(): Verifica se todos os caracteres da string estão em maiúsculas.
+##texto = "PYTHOn"
+##print(texto.isupper())
+##
+#### istitle(): Verifica se a string é um título (primeira letra
+#### de cada palavra em maiúscula).
+##texto = "Programação python"
+##print(texto.istitle())
+##
+#### title(): Converte o primeiro caractere de cada palavra da string para maiúscula.
+##texto = "olá, mundo!"
+##print(texto.title()) 
+####
+##
+#### swapcase(): Converte caracteres minúsculos em maiúsculos e vice-versa.
+##texto = "Python É Legal"
+##print(texto.swapcase())
+
+##""" lower(), upper(), capitalize(), casefold(), istitle(), title() e swapcase()"""
+##
+##texto = input("Digite um texto: ").upper()
+##print(texto)
+
+
+
