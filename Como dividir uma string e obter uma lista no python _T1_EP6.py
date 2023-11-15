e466d1c79135c68d9e22c387132f83f141c161f7
BrunoOJ <92693586+BrunoOJ@users.noreply.github.com>
2023-11-15 18:43:41 -0300

    Update Como dividir uma string e obter uma lista no python _T1_EP6.py

--- Como dividir uma string e obter uma lista no python _T1_EP6.py
+++ Como dividir uma string e obter uma lista no python _T1_EP6.py
@@ -3,12 +3,6 @@ PYTHON_STRING_T1_EP6 - Como dividir uma string e obter uma lista no python.
 
 '''
 
-
-
-# partition(sep): Divide a string em uma tupla (antes, separador, depois) com base no separador especificado.
-texto = "Python é uma linguagem de programação"
-print(texto.partition("é"))
-
 ### rsplit([sep[, maxsplit]]): Divide a string em uma lista de substrings usando o separador especificado, começando pela direita.
 ##texto = "maçã, laranja, banana"
 ##frutas = texto.rsplit(", ")
@@ -26,9 +20,4 @@ print(texto.partition("é"))
 ##"""
 ##linhas = texto.splitlines()
 ##print(linhas)
-##
-##
-### replace(): Substitui um determinado caractere ou sequência de caracteres por outro.
-##texto = "Olá, mundo!"
-##novo_texto = texto.replace("mundo", "Planeta Terra")
-##print(novo_texto)
+
